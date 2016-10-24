598f341e9d864f0caa41c190dd369c2c7c675268
Jonas Andreasson <jonas.m.andreasson@gmail.com>
2016-10-24 19:47:15 +0200

    File not found response page

--- server/src/server.js
+++ server/src/server.js
@@ -40,14 +40,14 @@ var https = require('https');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Add logger middleware
 app.use(require('./utils/request-logger'));
 
 // Serve client code as static assets
-if ( process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(express.static('../client/app'));
     app.use('/bower_components', express.static('../client/bower_components'));
 } else {
@@ -71,45 +71,62 @@ app.use('/api', require('./routers/about'));
 app.use('/api', authn.router);
 app.use('/api', require('./routers/config'));
 app.use('/api', require('./routers/controls'));
-//app.use('/api', require('./routers/positions'));
-//app.use('/api', require('./routers/entities'));
+
+// Handle page/route not found (404) 
+// =============================================================================
+app.use(function (req, res, next) {
+    res.status(404);
+    res.set('Content-Type', 'text/html');
+
+    if ( process.env.NODE_ENV === 'development' ) {
+        var options = {
+            root: __dirname + '../../../client/app/',
+        };
+    } else {
+        var options = {
+            root: __dirname + '../../../client/dist/',
+        };
+    }
+
+    res.sendFile('404.html', options)
+});
 
 // START THE HTTP(S) SERVER
 // =============================================================================
 var server = https.createServer({
-  key: fs.readFileSync('./src/ssl/key.pem'),
-  cert: fs.readFileSync('./src/ssl/cert.pem')
+    key: fs.readFileSync('./src/ssl/key.pem'),
+    cert: fs.readFileSync('./src/ssl/cert.pem')
 }, app).listen(port);
 
 logger.info('Web Server listening over SSL on ' + port);
 
-
+// 
 // SHUTDOWN THE APPLICATION WHEN ASKED TO
 // =============================================================================
 
 var shutdown = function () {
 
-  logger.info("Shuttting down API server")
-  server.close(function () {
-    logger.info("API Server closed sucessfully");
-    logger.info("Shutdown completed, exiting");
-    process.exit(0);
-  });
+    logger.info("Shuttting down API server");
+    server.close(function () {
+        logger.info("API Server closed sucessfully");
+        logger.info("Shutdown completed, exiting");
+        process.exit(0);
+    });
 
-  // if after
-  setTimeout(function () {
-    logger.error("Could not close connections in time, forcefully shutting down");
-    process.exit()
-  }, 10 * 1000);
+    // if after
+    setTimeout(function () {
+        logger.error("Could not close connections in time, forcefully shutting down");
+        process.exit();
+    }, 10 * 1000);
 };
 
 process.on('SIGTERM', function () {
-  logger.info("Got signal SIGTERM, shutting down");
-  shutdown();
+    logger.info("Got signal SIGTERM, shutting down");
+    shutdown();
 
 });
 
 process.on('SIGINT', function () {
-  logger.info("Got signal SIGINT, shutting down");
-  shutdown();
+    logger.info("Got signal SIGINT, shutting down");
+    shutdown();
 });
